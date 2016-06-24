e6cb8fb3d133f62beec111b3b19c2cc3a9373b87
Homerteo <mateo.ramirez.uribe@gmail.com>
2016-06-24 18:35:56 -0500

    se agregan los ultimos archivos

--- app.js
+++ app.js
@@ -6,7 +6,9 @@
                                                  'ui.bootstrap',
                                                  'ngStorage',
                                                  'toaster',
-                                                 'peopleManager.login']);
+                                                 'peopleManager.person',
+                                                 'peopleManager.login',
+                                                 'peopleManager.error']);
 
   // Ruta por default
   mainApp.run(['$state', function ($state) {
@@ -24,32 +26,27 @@
           })
           .state('newaccount', {
               url: '/login/new',
-              templateUrl: '/login/views/newAccount.html',
-              controller: 'loginController'
+              templateUrl: './components/login/views/newAccount.html',
+              controller: 'newUserController'
           })
           .state('newperson', {
               url: '/people/new',
-              templateUrl: '/person/views/newPerson.html',
-              controller: 'personController'
+              templateUrl: './components/person/views/newPerson.html',
+              controller: 'peopleController'
           })
-          .state('listpeople', {
+          .state('people', {
               url: '/people',
-              templateUrl: '/person/views/personList.html',
-              controller: 'personController'
-          })
-          .state('editperson', {
-            url: '/people/:id/edit',
-              templateUrl: '/person/views/editPerson.html',
-              controller: 'personController'
+              templateUrl: './components/person/views/personList.html',
+              controller: 'peopleController'
           })
           .state('personprofile', {
             url: '/people/:id',
-              templateUrl: '/person/views/showPerson.html',
-              controller: 'personController'
+              templateUrl: './components/person/views/showPerson.html',
+              controller: 'peopleController'
           })
           .state('error', {
             url: '/error',
-              templateUrl: '/error/views/error.html',
+              templateUrl: './components/error/views/error.html',
               controller: 'errorController'
           })
         $urlRouterProvider.otherwise('login');
